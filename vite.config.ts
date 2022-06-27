da7d65c07a454d9851e2de2eab543cee3a237dfd
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2022-06-27 22:21:02 +0100

    build: added build.minify to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [vue(), dts({
     entryRoot: resolve(__dirname, "libs")
   })],
@@ -16,7 +15,7 @@ export default defineConfig({
   },
 
   build: {
-    
+    minify: "esbuild",
     target: 'esnext',
     lib: {
       entry: resolve(__dirname, 'libs/index.ts'),
